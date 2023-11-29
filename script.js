0944e5b1f0dce2d56363317fd97f75900ac0f5d7
Srimon Anida <144817977+srimonloll@users.noreply.github.com>
2023-11-29 01:25:32 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+$(".mole1").click(function(){
+    $(".mole1").hide();
+});
+
+    
+$(".whack2").dblclick(function(){
+    $(".mole-two").toggle();
+});
+    
+$(".whack3").dblclick(function(){
+    $("img").show();
+    $(".third-mole").hide();
+});
+    
+$(".reset").click(function(){
+    $("img").show();
+});
+
+$(".yard1").click(function(){
+    $("img").show();
+});
+
